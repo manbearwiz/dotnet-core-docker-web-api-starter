2cb03aa3ef298c315136a71f3907047fa4865649
Kevin Brey <manbearwiz@gmail.com>
2016-09-27 14:54:38 -0500

    feat: add swagger

--- src/UserService/Startup.cs
+++ src/UserService/Startup.cs
@@ -34,6 +34,9 @@
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            // Inject an implementation of ISwaggerProvider with defaulted settings applied
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
@@ -47,6 +50,12 @@
             app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
+            app.UseSwaggerUi();
         }
     }
 }
